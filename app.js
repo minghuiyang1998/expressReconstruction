e450f1dc73073687fe59acdbec764b24afde4b4c
Yang Minghui <minghuiyang1998@gmail.com>
2018-10-14 22:13:25 +0800

    update(_method & put with image)

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 var helloWorld = require('./routes/helloworld')
 var app = express();
 
+var methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,15 +25,15 @@ app.use(express.static(path.join(__dirname, 'data')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/helloworld',helloWorld);
+app.use('/helloworld', helloWorld);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -2,125 +2,122 @@ var express = require('express');
 var fs = require('fs')
 var path = require('path')
 var multer = require('multer'); // v1.0.5
-var upload = multer({dest:"uploads/"}); // for parsing multipart/form-data
+var upload = multer({ dest: "uploads/" }); // for parsing multipart/form-data
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/article/new',function(req,res){
+router.get('/article/new', function (req, res) {
   console.log("new")
   res.render('new-article')
 })
 
-router.post('/articles',upload.any(),function(req,res){
+router.post('/articles', upload.any(), function (req, res) {
   var id = Date.now().toString()
-  console.log(req.files)
-  console.log(req.body)
   var data = {}
   data.title = req.body.title
   data.content = req.body.content
 
-  if(req.files){
+  if (req.files.length != 0) {
     var fileName = id + '_poster'
     data.file = fileName
-    // BUG:
-    // 没有传封面会错误 
     var filePath = req.files[0].path
-    console.log(data)
     var file = fs.readFileSync(filePath)
-    console.log(file)
-    fs.writeFileSync(path.resolve('data/images',fileName),file)
+    fs.writeFileSync(path.resolve('data/images', fileName), file)
   }
 
-  fs.writeFileSync(path.resolve('data/articles',id),JSON.stringify(data))
-  res.redirect("/article/"+id)
+  fs.writeFileSync(path.resolve('data/articles', id), JSON.stringify(data))
+  res.redirect("/article/" + id)
 })
 
-router.get('/article/:articleId',function(req,res){
-  console.log("article"+req.params.articleId)
+router.get('/article/:articleId', function (req, res) {
   var id = req.params.articleId
-  var article = fs.readFileSync(path.resolve('data/articles',id))
+  var article = fs.readFileSync(path.resolve('data/articles', id))
   var article_params = JSON.parse(article)
 
-  if(article_params.file){
-    article_params.file = "/images/"+article_params.file
+  if (article_params.file) {
+    article_params.file = "/images/" + article_params.file
   }
 
-  res.render('show-article',{params:article_params})
+  res.render('show-article', { params: article_params })
 
 })
 
-router.get('/articles',function(req,res){
+router.get('/articles', function (req, res) {
   var articles = fs.readdirSync("data/articles")
-  res.render('article-list',{articles:articles})
+  res.render('article-list', { articles: articles })
 })
 
-router.get('/article/:articleId/edit',function(req,res){
+router.get('/article/:articleId/edit', function (req, res) {
   var id = req.params.articleId
-  var article = fs.readFileSync(path.resolve('data/articles',id))
+  var article = fs.readFileSync(path.resolve('data/articles', id))
   var params = JSON.parse(article)
   params.id = id
-  res.render('modify-article',{params:params})
+  res.render('modify-article', { params: params })
 })
 
-router.put('/article/:articleId',upload.any(),function(req,res){
+router.put('/article/:articleId', upload.any(), function (req, res) {
+  console.log("PUT success")
   var id = req.params.articleId
-  console.log(id)
-  var data = {}
+  var data = {} 
   data.title = req.body.title
   data.content = req.body.content
- 
 
-  var article = fs.readFileSync(path.resolve('data/articles',id))
+  console.log(req.body)
+  console.log(req.files)
+
+  //读取原文件
+  var article = fs.readFileSync(path.resolve('data/articles', id))
   var article_params = JSON.parse(article)
 
   // 改完PUT的form 再来优化下，好像有点累赘
-  for(var item in data){
-    if(item != 'file' && data[item].length != 0 && article_params[item]!=data[item]){
+  for (var item in data) {
+    if (data[item].length != 0 && article_params[item] != data[item]) { 
       article_params[item] = data[item]
     }
-  }
-  
-  if(req.files){
-    
-    var fileName = id + '_poster'
-    data.file = fileName
-    var filePath = req.files[0].path
-    var file = fs.readFileSync(filePath)
-
-    if(file.length != 0){
-      fs.writeFileSync(path.resolve('data/images',fileName),file)
-      fs.unlinkSync(path.resolve('data/images',article_params.file))
-      article_params.file = data.file
-    }
-  }
-
-  console.log(data)
-  fs.writeFileSync(path.resolve('data/articles',id),JSON.stringify(article_params))
+  }  
+ 
   console.log(111)
-  
-  if(req.get('accept')==='text/html'){ // 这里好像没必要判断了吧，还要其他请求会响应吗
-    res.send("/article/"+id)
+  //如果上传了文件直接替换，不上传新图片data就没有file字段
+  if (req.files.length != 0) { 
+    var fileName = id + "_poster" 
+    data.file = fileName
+    var filePath = req.files[0].path 
+    var file = fs.readFileSync(filePath)
+    if (article_params.file) {
+      fs.unlinkSync(path.resolve('data/images', article_params.file))
+    }
+    fs.writeFileSync(path.resolve('data/images', fileName), file)
+    article_params.file = data.file
+  }
+ 
+  fs.writeFileSync(path.resolve('data/articles', id), JSON.stringify(article_params))
+
+  if(req.query['_method']) { 
+    res.redirect("/article/" + id)
+  }else {
+    res.send("/article/" + id)   
   }
 })
 
-router.delete('/article/:articleId',function(req,res){
+router.delete('/article/:articleId', function (req, res) {
   var id = req.params.articleId
-  var file = fs.readFileSync(path.resolve('data/articles',id))
+  var file = fs.readFileSync(path.resolve('data/articles', id))
   var params = JSON.parse(file)
 
-  if(params.file){
-    fs.unlinkSync(path.resolve('data/images',params.file))
+  if (params.file) {
+    fs.unlinkSync(path.resolve('data/images', params.file))
   }
 
-  fs.unlinkSync(path.resolve('data/articles',id))
+  fs.unlinkSync(path.resolve('data/articles', id))
   console.log(req.get('accept'))
-  if(req.get('accept')==="text/plain"){
+  if (req.accepts('text/plain') === 'text/plain') {
     console.log("delete")
     res.send("success")
   }
 })
+
 module.exports = router
