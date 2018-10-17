130bf04dc6e15f6e40a18ba50c0b75d06c1f749c
Yang Minghui <minghuiyang1998@gmail.com>
2018-10-17 23:04:01 +0800

    fix (使用callback，修改命名）

--- routes/index.js
+++ routes/index.js
@@ -40,49 +40,40 @@ router.post('/articles', upload.any(), function (req, res) {
 /*GET article/:articleId page */
 router.get('/article/:articleId', function (req, res) {
   var id = req.params.articleId
-  new Promise((resolve, reject) => {
-    db.serialize(function () {
-      db.all("SELECT * FROM Article WHERE id = $aid", { $aid: 1 }, function (err, article) {
-        resolve(article)
-      })
+
+  db.serialize(function () {
+    db.all("SELECT * FROM Article WHERE id = $aid", { $aid: 1 }, function (err, article) {
+      article_params = article[0]
+      // if (article_params.file) {
+      //   article_params.file = "/images/" + article_params.file
+      // }
+      res.render('show-article', { article_params: article_params })
     })
-  }).then((article) => {
-    console.log("article")
-    article_params = article[0]
-    // if (article_params.file) {
-    //   article_params.file = "/images/" + article_params.file
-    // }
-    res.render('show-article', { params: article_params })
+
   })
 })
 
 /*GET articles page */
 router.get('/articles', function (req, res) {
-  new Promise((resolve, reject) => {
-    db.serialize(function () {
-      db.all("SELECT * FROM Article", function (err, articles) {
-        resolve(articles)
-      })
+
+  db.serialize(function () {
+    db.all("SELECT * FROM Article", function (err, articles) {
+      console.log("articles")
+      console.log(articles)
+      res.render('article-list', { articles: articles })
     })
-  }).then((articles) => {
-    console.log("articles")
-    console.log(articles)
-    res.render('article-list', { articles: articles })
   })
 })
+
 
 /* GET article/:articleId/edit page */
 router.get('/article/:articleId/edit', function (req, res) {
   var id = req.params.articleId
-  new Promise((resolve, reject) => {
-    db.serialize(function () {
-      db.all('SELECT * FROM Article WHERE id = $aid', { $aid: 1 }, function (err, article) {
-        resolve(article)
-      })
+  db.serialize(function () {
+    db.all('SELECT * FROM Article WHERE id = $aid', { $aid: 1 }, function (err, article) {
+      var article_params = article[0]
+      res.render('modify-article', { article_params: article_params })
     })
-  }).then((article) => {
-    console.log(article)
-    res.render('modify-article', { params: article[0] })
   })
 })
 
@@ -98,49 +89,44 @@ router.put('/article/:articleId', upload.any(), function (req, res) {
   console.log(req.files)
 
   //读取原文件
-  new Promise((resolve, reject) => {
-    db.serialize(function () {
-      db.all("SELECT * FROM Article WHERE id = $aid", { $aid: 1 }, function (err, article) {
-        resolve(article)
-      })
+  db.serialize(function () {
+    db.all("SELECT * FROM Article WHERE id = $aid", { $aid: 1 }, function (err, article) {
+      console.log(article)
+
+      var article = fs.readFileSync(path.resolve('data/articles', id))
+      var article_params = JSON.parse(article)
+
+      // 改完PUT的form 再来优化下，好像有点累赘
+      for (var item in data) {
+        if (data[item].length != 0 && article_params[item] != data[item]) {
+          article_params[item] = data[item]
+        }
+      }
+
+      console.log(111)
+      //如果上传了文件直接替换，不上传新图片data就没有file字段
+      if (req.files.length != 0) {
+        var fileName = id + "_poster"
+        data.file = fileName
+        var filePath = req.files[0].path
+        var file = fs.readFileSync(filePath)
+        if (article_params.file) {
+          fs.unlinkSync(path.resolve('data/images', article_params.file))
+        }
+        fs.writeFileSync(path.resolve('data/images', fileName), file)
+        article_params.file = data.file
+      }
+
+      fs.writeFileSync(path.resolve('data/articles', id), JSON.stringify(article_params))
+      console.log(req.accepts('application/json') === 'application/json')
+      if (req.query['_method']) {
+        console.log('')
+        res.redirect("/article/" + id)
+      } else {
+        res.send("/article/" + id)
+      }
     })
-  }).then((article) => {
-    console.log(article)
-
-    var article = fs.readFileSync(path.resolve('data/articles', id))
-    var article_params = JSON.parse(article)
-  
-    // 改完PUT的form 再来优化下，好像有点累赘
-    for (var item in data) {
-      if (data[item].length != 0 && article_params[item] != data[item]) {
-        article_params[item] = data[item]
-      }
-    }
-  
-    console.log(111)
-    //如果上传了文件直接替换，不上传新图片data就没有file字段
-    if (req.files.length != 0) {
-      var fileName = id + "_poster"
-      data.file = fileName
-      var filePath = req.files[0].path
-      var file = fs.readFileSync(filePath)
-      if (article_params.file) {
-        fs.unlinkSync(path.resolve('data/images', article_params.file))
-      }
-      fs.writeFileSync(path.resolve('data/images', fileName), file)
-      article_params.file = data.file
-    }
-  
-    fs.writeFileSync(path.resolve('data/articles', id), JSON.stringify(article_params))
-    console.log(req.accepts('application/json') === 'application/json')
-    if (req.query['_method']) {
-      console.log('')
-      res.redirect("/article/" + id)
-    } else {
-      res.send("/article/" + id)
-    }    
   })
-
 })
 
 /*DELETE article/:articleId */
@@ -163,3 +149,4 @@ router.delete('/article/:articleId', function (req, res) {
 })
 
 module.exports = router
+  
