351e8d87c59f3d96b77e25d764b4c79aed9ac0d0
Yang Minghui <minghuiyang1998@gmail.com>
2018-10-19 23:55:00 +0800

    fix(register)

--- routes/users.js
+++ routes/users.js
@@ -9,21 +9,32 @@ const salt = '1234567'
 
 /* GET users listing. */
 router.get('/join',function(req,res){
-  res.render('register')
+  res.render('register',{err:null})
 })
 
 router.post('/join', function(req, res) {
   console.log(req.body)
   var password = md5(md5(req.body.password)+salt)
   db.serialize(function () {
-    db.run("INSERT INTO User(email,password) values($email, $password)",
+    db.get("SELECT * FROM User WHERE email = $email", { $email: req.body.email }, function (err, data) {
+      if (err) throw err
+      console.log(data)
+      if(data)
       {
-        $email: req.body.email,
-        $password: password
-      }, function (err) {
-        if (err) throw err
-        res.send("success!")
-      })
+        res.render('register',{err:"fail! Email is already taken"})
+      }else{
+        db.serialize(function () {
+          db.run("INSERT INTO User(email,password) values($email, $password)",
+            {
+              $email: req.body.email,
+              $password: password
+            }, function (err) {
+              if (err) throw err
+              res.send("success!")
+            })
+        })
+      }
+    })
   })
 });
 
