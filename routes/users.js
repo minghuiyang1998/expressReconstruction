174eeaafbfe537db0715083e0eec35c100c73b06
Yang Minghui <minghuiyang1998@gmail.com>
2018-10-20 00:16:56 +0800

    fix(join)

--- routes/users.js
+++ routes/users.js
@@ -8,48 +8,39 @@ const salt = '1234567'
 
 
 /* GET users listing. */
-router.get('/join',function(req,res){
-  res.render('register',{err:null})
+router.get('/join', function (req, res) {
+  res.render('register', { err: null })
 })
 
-router.post('/join', function(req, res) {
+router.post('/join', function (req, res) {
   console.log(req.body)
-  var password = md5(md5(req.body.password)+salt)
-  db.serialize(function () {
-    db.get("SELECT * FROM User WHERE email = $email", { $email: req.body.email }, function (err, data) {
-      if (err) throw err
-      console.log(data)
-      if(data)
-      {
-        res.render('register',{err:"fail! Email is already taken"})
-      }else{
-        db.serialize(function () {
-          db.run("INSERT INTO User(email,password) values($email, $password)",
-            {
-              $email: req.body.email,
-              $password: password
-            }, function (err) {
-              if (err) throw err
-              res.send("success!")
-            })
-        })
-      }
-    })
-  })
-});
+  var password = md5(md5(req.body.password) + salt)
 
-router.get('/',function(req,res){
+  db.serialize(function () {
+    db.run("INSERT INTO User(email,password) values($email, $password)",
+      {
+        $email: req.body.email,
+        $password: password
+      }, function (err) {
+        if (err) {
+          res.render('register',{err:"Fail!This email has been taken"}) 
+        }
+        res.send("success!")
+      })
+  })
+})
+
+router.get('/', function (req, res) {
   res.render('login')
 })
 
-router.post('/',function(req,res){
+router.post('/', function (req, res) {
   db.serialize(function () {
     db.get("SELECT password FROM User WHERE email = $email", { $email: req.body.email }, function (err, data) {
       if (err) throw err
-      if(md5(md5(req.body.password)+salt) === data.password)
-      {
-        res.render('index',{title:'Hello '+req.body.email})
-      }else{
+      if (md5(md5(req.body.password) + salt) === data.password) {
+        res.render('index', { title: 'Hello ' + req.body.email })
+      } else {
         res.send(400)
       }
     })
